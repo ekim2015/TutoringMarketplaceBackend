f6d94ebf08786599145fb7476325cab2d99ad004
ekim2015 <elliot.kim2015@gmail.com>
2025-12-22 12:09:10 -0800

    Security + web socket configs

--- src/main/java/com/ekim/tutoring/config/AuthenticationConfig.java
+++ src/main/java/com/ekim/tutoring/config/AuthenticationConfig.java
@@ -1,14 +1,8 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,25 +10,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class AuthenticationConfig  {
 
     @Bean
-    public UserDetailsService userDetailsService(BCryptPasswordEncoder encoder) {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        
-        if (manager.userExists("user")) {
-            return manager;
-        } else {
-            manager.createUser(User.withUsername("admin")
-            .password(encoder.encode("password"))
-            .roles("USER" , "ADMIN")
-            .build());
-
-            return manager;
-        }
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(Customizer.withDefaults())
+            .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/tutor/**").hasRole("tutor")
+                .requestMatchers("/customer/**").hasRole("customer")
+                .requestMatchers("/user/**").hasRole("user")
+                .requestMatchers("/admin/**").hasRole("admin")
+                .anyRequest().authenticated())
+            .oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(null));
+        return http.build();
     }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) {
-        
-    }
-
 
 }
