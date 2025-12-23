75901df5ff799d3bf3a466a62305b426ab7e1708
ekim2015 <elliot.kim2015@gmail.com>
2025-12-23 12:53:31 -0800

    Implemented some methods and did some refactoring

--- src/main/java/com/ekim/tutoring/controllers/TutorController.java
+++ src/main/java/com/ekim/tutoring/controllers/TutorController.java
@@ -1,11 +1,14 @@
 package com.ekim.tutoring.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +22,15 @@ public class TutorController {
     private TutorService tutorService;
 
     @GetMapping(value="/{tutorUser}")
-    public ResponseEntity<Tutor> getTutorDetails(@PathVariable String tutorUser) {
-    
+    public ResponseEntity<Tutor> getTutorDetails(@PathVariable String tutorUser, @RequestBody Tutor tutor) {
+        Optional<Tutor> foundTutor = tutorService.getTutorByUser(tutorUser);
+        if (foundTutor.isPresent()) {
+            return new ResponseEntity<>(foundTutor.get(), HttpStatus.FOUND);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
+    
 
 }
--- src/main/java/com/ekim/tutoring/services/UserService.java
+++ src/main/java/com/ekim/tutoring/services/UserService.java
@@ -1,8 +1,10 @@
 package com.ekim.tutoring.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.ekim.tutoring.models.User;
 import com.ekim.tutoring.repositories.UserRepository;
 
 // solely used for registration and login
@@ -10,5 +12,14 @@ import com.ekim.tutoring.repositories.UserRepository;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public void createUser(User newUser) {
+        String newPassword = new BCryptPasswordEncoder().encode(newUser.getPassword());
+        
+        User registeredUser = new User(newUser.getUsername(), newPassword, newUser.getPhoneNumber());
+        userRepository.save(registeredUser);
+    }
+
+
     
 }
